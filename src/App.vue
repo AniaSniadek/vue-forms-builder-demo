4943a70b48ba0e5b40c884105f97b0c2dfaa1de7
AniaSniadek <annsni2@st.amu.edu.pl>
2022-05-09 17:51:49 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { FormBuilder, Validators } from 'vue-forms-builder';
+
+const control = FormBuilder.control(null, Validators.required);
+</script>
+
+<template>
+  <input type="text" v-model="control.value" />
+  {{ control.value }}
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
